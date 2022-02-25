e4ce64496de50a647c9a189b5eb8a74f57929de0
marcusvbrangel <marcus.vbrangel@gmail.com>
2022-02-25 01:12:39 -0300

    feat: add use case delete account and balance account

--- 02-primeiro-projeto-com-node/src/index.js
+++ 02-primeiro-projeto-com-node/src/index.js
@@ -160,4 +160,25 @@ app.get("/account", verifyIfAccoutExistsByCPF, (request, response) => {
 
 });
 
+app.delete("/account", verifyIfAccoutExistsByCPF, (request, response) => {
+
+  const { customer } = request;
+
+  // splice
+  customers.splice(customer, 1);
+
+  return response.status(200).json(customers);
+
+});
+
+app.get("/balance", verifyIfAccoutExistsByCPF, (request, response) => {
+
+  const { customer } = request;
+
+  const balance = getBalance(customer.statement);
+
+  return response.json(balance);
+
+});
+
 app.listen(3333);
